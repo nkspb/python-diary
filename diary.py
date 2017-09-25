e174e922de0e0c7315eaf3ce1593c7a6bbe8c331
Nikolay <nkspb13@gmail.com>
2017-09-25 16:41:15 +0300

    Created add entry option

--- diary.py
+++ diary.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import datetime, sys
+from collections import OrderedDict
 
 from peewee import *
-import datetime
+
 
 db = SqliteDatabase('diary.db')
 
@@ -23,15 +25,45 @@ def initialize():
     
 def menu_loop():
   '''Show the menu'''
-  
+  # we haven't chosen anything when we run the app
+  choice = None
+  # show menu until user enteres q
+  while choice != 'q':
+    print("Enter 'q' to quit.")
+    # go through menu dict
+    for key, value in menu.items():
+      # print menu items
+      print('{}) {}'.format(key, value.__doc__))
+      
+    choice = input('Action: ').lower().strip()
+    
+    # perform selected action
+    # as value is a function, we call it with ()
+    if choice in menu:
+      menu[choice]()
+      
 def add_entry():
   '''Add an entry.'''
-  
+  print("Enter your entry. Press Ctrl-D when finished.")
+  #sys.stdin accepts data until EOF
+  data = sys.stdin.read().strip()
+  #if user entered some data, offer to save it
+  if data:
+    if input('Save entry? [Yn] ').lower() != 'n':
+      # create a row with data
+      Entry.create(content=data)
+      print("Saved successfully!")
+      
 def view_entries():
   '''View previous entries.'''
   
 def delete_entry(entry):
   '''Delete an entry.'''
+
+menu = OrderedDict([
+  ('a', add_entry),
+  ('v', view_entries)    
+])
   
 if __name__ == '__main__':
   initialize()
